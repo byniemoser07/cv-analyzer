b85e558931ff20d17ed247e2531127b824f90449
byniemoser07 <akarshitsingh0502@gmail.com>
2026-08-03 11:23:08 +0530

    Use deployed backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,33 +1,32 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
-    timeout: 120000,
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 120000,
 });
 
 export const analyzeResume = async (
-    file,
-    jobDescription,
-    githubUsername
+  file,
+  jobDescription,
+  githubUsername
 ) => {
+  const formData = new FormData();
 
-    const formData = new FormData();
+  formData.append("file", file);
+  formData.append("job_description", jobDescription);
+  formData.append("github_username", githubUsername);
 
-    formData.append("file", file);
-    formData.append("job_description", jobDescription);
-    formData.append("github_username", githubUsername);
+  const response = await API.post(
+    "/analyze-profile",
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
 
-    const response = await API.post(
-        "/analyze-profile",
-        formData,
-        {
-            headers: {
-                "Content-Type": "multipart/form-data",
-            },
-        }
-    );
-
-    return response.data;
+  return response.data;
 };
 
 export default API;
